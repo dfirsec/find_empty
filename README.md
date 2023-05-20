169d0eeecf9ecf463b77ff43d8bd6fcfb6637dfa
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-05-20 08:55:33 -0400

    Updated

--- README.md
+++ README.md
@@ -1,12 +1,77 @@
 # Find Empty
 
-![Generic badge](https://img.shields.io/badge/python-3.8-blue.svg) [![Twitter](https://img.shields.io/badge/Twitter-@pulsecode-blue.svg)](https://twitter.com/pulsecode)
+![Generic badge](https://img.shields.io/badge/python-3.8-blue.svg)
 
-Python script to scan for empty directories.
+Python script to to recursively scan a directory and find empty directories within it. It generates a list of empty directory paths and writes them to a text file named `empty_dirs.txt`.
+
+## Prerequisites
+
+Python 3.x
+
+## Installation
+
+1. Clone the repository:
 
 ```text
-python find_empty.py d:\
-> Scanning for empty directories: 18360 files [00:08, 2212.80 files/s]
-> Found 17 emtpy directories.
-> Results written to: D:\find_empty\empty_dirs.txt
+git clone https://github.com/dfirsec/find_empty.git
 ```
+
+2. Navigate to the project directory:
+
+```text
+cd find_empty
+```
+
+3. Install the required dependencies using poetry:
+
+```text
+poetry install
+```
+
+## Usage
+
+1. Create the virtual environment
+
+```text
+poetry shell
+```
+
+2. Run the program
+
+```text
+python find_empty.py <Directory path to scan>
+```
+
+3. Exit the program when done.
+
+```text
+exit
+```
+
+> Replace \<Directory path to scan> with the path to the directory you want to scan for empty directories.
+
+### Options
+
+--exclude \<Directory name>: Exclude a specific directory from the scanning process. You can use this option multiple times to exclude multiple directories.
+
+## Output
+
+The program will scan the provided directory and display the progress using a progress bar. Once the scan is complete, it will generate a text file named empty_dirs.txt that contains the paths of all empty directories found during the scan.
+
+If no empty directories are found, the program will display a message indicating that no empty directories were found.
+
+## Example
+
+```text
+python find_empty.py /path/to/directory --exclude Desktop --exclude Windows
+```
+
+This command will scan the /path/to/directory directory for empty directories, excluding the directories named "Desktop" and "Windows". It will display the progress and generate the empty_dirs.txt file containing the paths of the empty directories.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please create an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
--- find_empty.py
+++ find_empty.py
@@ -1,60 +1,72 @@
+"""Find empty directories."""
+
 import os
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
-AUTHOR = "DFIRSec (@pulsecode)"
-VERSION = "v0.0.1"
-DESCRIPTION = "Find empty directories"
-
-try:
-    from tqdm import tqdm
-except ImportError:
-    sys.exit("\n\033[91m[ERROR]\033[0m Please install tqdm: 'pip install tqdm --user'\n")  # fmt: skip
+from tqdm import tqdm
 
 
-def main(dirpath):
-    """
-    Recursively scans a directory for empty directories and writes the results to a text file.
+def main(dirpath: str, exclusions: set) -> None:
+    """Recursively scans a source for empty directories.
 
-    :param dirpath: The path to the directory you want to scan
+    Args:
+        dirpath (str): Directory path to scan.
+        exclusions (set): Set of directories to exclude from scan.
     """
     parent = Path(__file__).resolve().parent
     outfile = parent.joinpath("empty_dirs.txt")
-    exclude = {"Windows", "Desktop"}
     empty = []
-    cnt = 0
-    try:
-        for root, dirs, files in tqdm(
-            os.walk(dirpath, topdown=True),
-            desc="\033[33m> Scanning for empty directories\033[0m",
-            unit=" files",
-        ):
-            dirs[:] = [d for d in dirs if not d.startswith(".") and d not in exclude]
-            if not files and not dirs:
-                empty.append(root)
-                cnt += 1
-                #! CAUTION: Uncomment 4 lines below to remove empty dirs
-                # try:
-                #     Path.rmdir(dirpath)
-                # except OSError as e:
-                #     print(f'Error: {dirpath} : {e.strerror}')
-        if cnt:
-            print(f"> Found {cnt:,} emtpy directories.")
-            with open(outfile, "w", encoding="utf-8") as fileobj:
-                for filepath in empty:
-                    fileobj.write(f"{filepath}\n")
-            print(f"> Results written to: \033[96m{Path(outfile).resolve()}\033[0m")
-        else:
-            print("> Found no emtpy directories.")
-    except KeyboardInterrupt:
-        os.remove(outfile)  # remove unfinished output file
-        sys.exit("\033[92m> Script Terminated!\033[0m")
+    counter = 0
+
+    for root, dirs, files in tqdm(
+        os.walk(dirpath, topdown=True),
+        desc="\033[33m> Scanning for empty directories\033[0m",
+        unit=" files",
+    ):
+        dirs[:] = [d for d in dirs if not d.startswith(".") and d not in exclusions]
+        if not files and not dirs:
+            empty.append(root)
+            counter += 1
+            #! CAUTION: Uncomment 4 lines below to remove empty dirs
+            # try:
+            #     Path.rmdir(dirpath)
+            # except OSError as e:
+            #     print(f'Error: {dirpath} : {e.strerror}')
+
+    if counter:
+        print(f"> Found {counter:,} empty directories.")
+        with open(outfile, "w", encoding="utf-8") as fileobj:
+            fileobj.writelines(f"{filepath}\n" for filepath in empty)
+        print(f"> Results written to: \033[96m{Path(outfile).resolve()}\033[0m")
+    else:
+        print("> Found no empty directories.")
+
+
+def parse_arguments() -> tuple[str, set]:
+    """Parse command-line arguments.
+
+    Returns:
+        A tuple containing the directory path and the set of excluded directories.
+    """
+    parser = ArgumentParser()
+    parser.add_argument("dirpath", help="Directory path to scan")
+    parser.add_argument(
+        "--exclude",
+        action="append",
+        help="Directories to exclude from the scan",
+        default=["Desktop", "Windows"],
+    )
+    args = parser.parse_args()
+    dirpath = args.dirpath
+    exclusions = set(args.exclude)
+    return dirpath, exclusions
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or not Path(sys.argv[1]).exists():
+    dirpath, exclusions = parse_arguments()
+    if not Path(dirpath).exists():
         sys.exit("\033[91m[ERROR]\033[0m Please include a valid path.")
-    else:
-        path = sys.argv[1]
 
-    main(path)
+    main(dirpath, exclusions)
